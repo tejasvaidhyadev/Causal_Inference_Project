d07e0b571f06280087bb2679c678389369bd4a6c
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2023-04-27 01:48:00 -0400

    fixing gen_dataset.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Causal Infernence course project
 Unofficial repository for the the paper ["Counterfactual Invariance to Spurious Correlations: Why and How to Pass Stress Tests"](https://arxiv.org/pdf/2106.00545.pdf) 
 
+# Generate Datasets
+gen_dataset only supports generating synthetic datasets for now. Although I have added script for generating real datasets, it is not yet ready for use. checkout for induce_dependence.py for more details.
+
 # Warning
 The code is not yet ready for public use. We are working on it.
 Do not use it unless you know what you are doing.
--- gen_dataset.py
+++ gen_dataset.py
@@ -1,56 +1,66 @@
+# This implementation is from "Robustness to Stress Tests synthetic data"
+# This looks like potentially a good way to induce dependence in a dataset
+
 import numpy as np
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
-#===============================================================================
-# BASED ON Synthetic Counterfactuals in Product Review Data FROM PAPER.
-# script try to replicate figure 2 from the paper
-# Load the Amazon reviews data
-#===============================================================================
-
-reviews = pd.read_csv('data/amazon_reviews.csv')
-
-# Select 20000 product reviews and assign labels based on the star rating
-reviews_subset = reviews.sample(n=20000, random_state=42)
-Y = np.where(reviews_subset['score'].isin([4, 5]), 1, 0)
-
-# Use only the first 20 tokens of text
-X = reviews_subset['text'].str.split().str[:20].str.join(' ')
-
-# Assign Z as a Bernoulli random variable
-Z = np.random.binomial(1, 0.5, size=len(X))
-
-# Replace the tokens "and" and "the" based on the value of Z
-X = np.where(Z == 1, X.str.replace('and', 'andxxxxx').str.replace('the', 'thexxxxx'), 
-             X.str.replace('and', 'andyyyyy').str.replace('the', 'theyyyyy'))
-
-# Resample the data to induce a dependency between Y and Z
-gamma = 0.3
-p_Y = 0.5
-n_Y = int(len(Y) * p_Y)
-n_Z = int(len(Z) * gamma)
-idx_Y1Z1 = np.where((Y == 1) & (Z == 1))[0]
-idx_Y1Z0 = np.where((Y == 1) & (Z == 0))[0]
-idx_Y0Z1 = np.where((Y == 0) & (Z == 1))[0]
-idx_Y0Z0 = np.where((Y == 0) & (Z == 0))[0]
-idx_Y1Z1_resampled = np.random.choice(idx_Y1Z1, size=int(n_Y*n_Z*p_Y), replace=True)
-idx_Y1Z0_resampled = np.random.choice(idx_Y1Z0, size=int(n_Y*(1-gamma)*p_Y), replace=True)
-idx_Y0Z1_resampled = np.random.choice(idx_Y0Z1, size=int((1-p_Y)*n_Z*gamma), replace=True)
-idx_Y0Z0_resampled = np.random.choice(idx_Y0Z0, size=int((1-p_Y)*(1-gamma)*n_Z), replace=True)
-idx_resampled = np.concatenate([idx_Y1Z1_resampled, idx_Y1Z0_resampled, idx_Y0Z1_resampled, idx_Y0Z0_resampled])
-X_resampled = X[idx_resampled]
-Y_resampled = Y[idx_resampled]
-Z_resampled = Z[idx_resampled]
-
-# Split the data into training and testing sets
-X_train, X_test, Y_train, Y_test, Z_train, Z_test = train_test_split(X_resampled, Y_resampled, Z_resampled, 
-                                                                    test_size=0.2, random_state=42)
-
-# save the dataset to csv
-# check and create data folder
 import os
-if not os.path.exists('data'):
-    os.makedirs('data')
 
-pd.DataFrame({'X': X_train, 'Y': Y_train, 'Z': Z_train}).to_csv('data/train.csv', index=False)
-pd.DataFrame({'X': X_test, 'Y': Y_test, 'Z': Z_test}).to_csv('data/test.csv', index=False)
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def preprocess_data(reviews, n_samples=10000):
+    reviews_subset = reviews.sample(n=n_samples, random_state=42)
+    Y = np.where(reviews_subset['score'].isin([4, 5]), 1, 0)
+    X = reviews_subset['text'].str.split().str[:20].str.join(' ')
+    
+    return X, Y
+
+def assign_Z_variable(X):
+    Z = np.random.binomial(1, 0.5, size=len(X))
+    return Z
+
+def replace_tokens(X, Z):
+    X = np.where(Z == 1, X.str.replace('and', 'andxxxxx').str.replace('the', 'thexxxxx'), 
+                 X.str.replace('and', 'andyyyyy').str.replace('the', 'theyyyyy'))
+    return X
+
+def resample_data(X, Y, gamma=0.3, p_Y=0.5):
+    X_resampled = np.random.permutation(X)
+    Y_resampled = np.random.binomial(1, p_Y, size=len(Y))
+    Z_resampled = np.where(Y_resampled == 1, np.random.binomial(1, gamma, size=len(Y)),
+                              np.random.binomial(1, 1 - gamma, size=len(Y)))
+    
+    return X_resampled, Y_resampled, Z_resampled
+
+def split_data(X, Y, Z, test_size=0.2):
+    return train_test_split(X, Y, Z, test_size=test_size, random_state=42)
+
+def save_data_to_csv(X, Y, Z, file_path):
+    if not os.path.exists('data'):
+        os.makedirs('data')
+        
+    pd.DataFrame({'X': X, 'Y': Y, 'Z': Z}).to_csv(file_path, index=False)
+
+def create_perturbed_dataset(test_df):
+    perturbed_df = test_df.copy()
+    perturbed_df['X'] = perturbed_df['X'].str.replace('yyyyy', '').str.replace('xxxxx', '')
+    perturbed_df['X'] = np.where(perturbed_df['Z'] == 1, 
+                                  perturbed_df['X'].str.replace('and', 'andyyyyy').str.replace('the', 'theyyyyy'),
+                                  perturbed_df['X'].str.replace('and', 'andxxxxx').str.replace('the', 'thexxxxx'))
+    return perturbed_df
+
+if __name__ == "__main__":
+    reviews = load_data('data/amazon_reviews.csv')
+    X, Y = preprocess_data(reviews)
+    Z = assign_Z_variable(X)
+    X = replace_tokens(X, Z)
+    X_resampled, Y_resampled, Z_resampled = resample_data(X, Y)
+    X_train, X_test, Y_train, Y_test, Z_train, Z_test = split_data(X_resampled, Y_resampled, Z_resampled)
+    
+    save_data_to_csv(X_train, Y_train, Z_train, 'data/train.csv')
+    save_data_to_csv(X_test, Y_test, Z_test, 'data/test.csv')
+    
+    test_df = load_data('data/test.csv')
+    perturbed_df = create_perturbed_dataset(test_df)
+    perturbed_df.to_csv('data/perturbed_test.csv', index=False)
